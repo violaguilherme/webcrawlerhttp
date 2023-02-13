8138f85919d5039e10da8f94e1bcd8fc9daf3b9e
violaguilherme <guilherme_schott@hotmail.com>
2023-02-13 15:03:57 -0300

    normalize url's and tests run

---
+++ crawl.js
@@ -0,0 +1,13 @@
+function normalizeURL(urlString) {
+    const urlObject = new URL(urlString)
+    const hostPath = `${urlObject.hostname}${urlObject.pathname}`
+
+    if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+        return hostPath.slice(0, -1)
+    }
+        return hostPath 
+}
+
+module.exports = {
+    normalizeURL
+}
