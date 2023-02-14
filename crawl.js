4656cd6ed0b0cea32cc54cedd9d1c812a60972e8
violaguilherme <guilherme_schott@hotmail.com>
2023-02-14 09:44:02 -0300

    crawl a single page and handle error

--- crawl.js
+++ crawl.js
@@ -1,5 +1,30 @@
 const { JSDOM } = require("jsdom")
 
+async function crawlPage(currentURL) {
+    console.log(`Actively crawling: ${currentURL}`)
+
+    try {
+        const response = await fetch(currentURL)
+
+        if (response.status > 399) {
+            console.log(`Error with status ${response.status} on page ${currentURL}`)
+            return
+        }
+
+        const contentType = response.headers.get("content-type")
+
+        if (!contentType.includes("text/html")) {
+            console.log(`Non HTML response, content type ${contentType} don't exist on page ${currentURL}`)
+            return
+        }
+
+        console.log(await response.text())
+    } catch(err) {
+        console.log(`Error: ${err.message} on page ${currentURL}`)
+    }
+
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -34,5 +59,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+const { crawlPage } = require("./crawl")
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("No website provided")
+        process.exit(1)
+    }
+
+    if (process.argv.length > 3) {
+        console.log("Just one website can be provided")
+        process.exit(1)
+    }
+    const baseUrl = process.argv[2]
+
+    console.log(`Starting crawl of ${baseUrl}`)
+
+    crawlPage(baseUrl)
+}
+
+main()
